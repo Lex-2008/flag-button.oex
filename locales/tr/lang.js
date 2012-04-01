df7026acb7f4ded906b659c840db442aa4a17492
Aleksei Shpakovskii <alekseis@opera.com>
2012-04-01 19:18:00 +0200

    version 1.6-dev2

--- locales/tr/lang.js
+++ locales/tr/lang.js
@@ -12,4 +12,6 @@
     defWidth:200,
     confirmReset:'Tüm ayarları varsayılan ayarlara döndürmek istediğinizden emin misiniz?',
     offlineText:'Data is unavailable in offline mode',
+    dateFormat:'%month%, %day%',
+    dateMonths:['Jan','Feb','March','Apr','May','June','July','Aug','Sept','Oct','Nov','Dec'],
     };
